0a0fbaab1ad13b9201658744e965006ee7a01e1a
Juan Miguel <oliveirajuan243@gmail.com>
2026-02-14 11:08:57 -0300

    conexão sql server executada com sucesso

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'sqlsrv'),
 
     /*
     |--------------------------------------------------------------------------
@@ -94,20 +94,24 @@ return [
             'sslmode' => 'prefer',
         ],
 
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
-        ],
+            'sqlsrv' => [
+                'driver' => 'sqlsrv',
+                'host' => env('DB_HOST', 'localhost'),
+                'port' => env('DB_PORT', '1433'),
+                'database' => null,
+                'username' => null,
+                'password' => env('DB_PASSWORD', ''),
+                'charset' => 'utf8',
+                'prefix' => '',
+                'encrypt' => 'yes',
+                'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', false),
+                'options' => [
+                    'TrustServerCertificate' => true,
+                    'Encrypt' => false,
+                ],
+
+            ],
+
 
     ],
 
